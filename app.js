6b2064890cec9cad54adb21c839a60c71362eb97
Eloi Pardo <eloipr94@gmail.com>
2020-01-28 19:18:02 +0100

    add cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
@@ -15,6 +16,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/expenses", expensesRouter);
 app.use("/users", usersRouter);
